dd3ade6c656d3b662b6f7074d5c99902c2f4a5f0
Gayathri <gayathri@gmail.com>
2026-03-06 15:11:14 +0530

    fix github pages

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template, request, redirect, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("upload.html")
-
 # --------------------
 # LOGIN PAGE (MAIN LINK)
 # --------------------
